be2900946f1bc48d3794dee0bb2497d6969de133
ConorTest <50129082@belfastmet.ac.uk>
2026-02-12 16:17:02 +0000

    robot REALREALFIXED

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ myText5 = "|  (===)  |<br>"
 myText6 = "|_________|<br>"
 myText7 = "BEEP BOOP"
 
+myTextFinal = myText1+myText2+myText3+myText4+myText5+myText6+myText7
 @app.route('/')
 def hello_world():
-    return myText7
+    return myTextFinal
