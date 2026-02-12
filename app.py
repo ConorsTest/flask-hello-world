667f026637f0ef8e2136d4d21db721dce36bd76b
ConorTest <50129082@belfastmet.ac.uk>
2026-02-12 15:54:38 +0000

    robot REAL

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ myText5 = "|  (===)  |\n"
 myText6 = "|_________|\n"
 myText7 = "BEEP BOOP"
 
+myTextFinal = myText1+myText2+myText3+myText4+myText5+myText6+myText7
+
 @app.route('/')
 def hello_world():
-    return ':)'
+    return 
