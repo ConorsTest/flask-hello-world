7abe6724a2f0dd7d664c9083704be1c8178963c8
ConorTest <50129082@belfastmet.ac.uk>
2026-02-12 16:20:44 +0000

    robot REALREALFIXEDdithering

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 myText1 = "__________<br>"
-myText2 = "|         |<br>"
-myText3 = "|  0   0  |<br>"
-myText4 = "|         |<br>"
-myText5 = "|  (===)  |<br>"
+myText2 = "|.........|<br>"
+myText3 = "|..0 ..0..|<br>"
+myText4 = "|.........|<br>"
+myText5 = "|..(===)..|<br>"
 myText6 = "|_________|<br>"
 myText7 = "BEEP BOOP"
 
