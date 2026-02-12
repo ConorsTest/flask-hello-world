f34a76048b45e84ead716c4b969ff6347e08194f
ConorTest <50129082@belfastmet.ac.uk>
2026-02-12 15:51:42 +0000

    robot

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+myText1 = "__________\n"
+myText2 = "|         |\n"
+myText3 = "|  0   0  |\n"
+myText4 = "|         |\n"
+myText5 = "|  (===)  |\n"
+myText6 = "|_________|\n"
+myText7 = "BEEP BOOP"
+
 @app.route('/')
 def hello_world():
     return ':)'
