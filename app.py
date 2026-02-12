7400c43157ca5383f20e34b148a73cc7a8ddb757
ConorTest <50129082@belfastmet.ac.uk>
2026-02-12 16:10:48 +0000

    robot REALREALFIXED

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-myText1 = "__________\n"
-myText2 = "|         |\n"
-myText3 = "|  0   0  |\n"
-myText4 = "|         |\n"
-myText5 = "|  (===)  |\n"
-myText6 = "|_________|\n"
+myText1 = "__________<br>"
+myText2 = "|         |<br>"
+myText3 = "|  0   0  |<br>"
+myText4 = "|         |<br>"
+myText5 = "|  (===)  |<br>"
+myText6 = "|_________|<br>"
 myText7 = "BEEP BOOP"
 
 @app.route('/')
 def hello_world():
-    print(myText1)
-    print(myText2)
-    print(myText3)
-    print(myText4)
-    print(myText5)
-    print(myText6)
     return myText7
